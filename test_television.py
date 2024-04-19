5ad8616472c6e6cb46cb0ac37f6275897d620244
DrFaustest <scottaf80234@gmail.com>
2024-04-19 10:23:09 -0500

    Add dependencies for pip, pluggy, pytest, colorama, packaging, iniconfig, setuptools, idna, distro, urllib3, and models

--- test_television.py
+++ test_television.py
@@ -1,90 +1,85 @@
 import pytest
 from television import Television
 
-def test_init():
-    tv = Television()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+class Test:
+    def setup_method(self):
+        self.tv = Television()
 
-def test_power():
-    tv = Television()
-    tv.power()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-    tv.power()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+    def teardown_method(self):
+        del self.tv
 
+    def test_init(self):
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
 
-def test_mute():
-    tv = Television()
-    tv.power()
-    tv.volume_up()
-    tv.mute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-    tv.mute()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
-    tv.power()
-    tv.mute()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 1"
-    tv.mute()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 1"
+    def test_power(self):
+        self.tv.power()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
+        self.tv.power()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
 
+    def test_mute(self):
+        self.tv.power()
+        self.tv.volume_up()
+        self.tv.mute()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
+        self.tv.mute()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 1"
+        self.tv.power()
+        self.tv.mute()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 1"
+        self.tv.mute()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 1"
 
-def test_channel_up():
-    tv = Television()
-    tv.channel_up()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
-    tv.power()
-    tv.channel_up()
-    assert str(tv) == "Power = True, Channel = 1, Volume = 0"
-    for _ in range(3):
-        tv.channel_up()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    def test_channel_up(self):
+        self.tv.channel_up()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.channel_up()
+        assert str(self.tv) == "Power = True, Channel = 1, Volume = 0"
+        for _ in range(3):
+            self.tv.channel_up()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
 
+    def test_channel_down(self):
+        self.tv.channel_down()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.channel_down()
+        assert str(self.tv) == "Power = True, Channel = 3, Volume = 0"
+        for _ in range(3):
+            self.tv.channel_down()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
 
-def test_channel_down():
-    tv = Television()
-    tv.channel_down()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
-    tv.power()
-    tv.channel_down()
-    assert str(tv) == "Power = True, Channel = 3, Volume = 0"
-    for _ in range(3):
-        tv.channel_down()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    def test_volume_up(self):
+        self.tv.volume_up()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.volume_up()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 1"
+        self.tv.mute()
+        self.tv.volume_up()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 2"
+        for _ in range(3):
+            self.tv.volume_up()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 2"
 
-
-def test_volume_up():
-    tv = Television()
-    tv.volume_up()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
-    tv.power()
-    tv.volume_up()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
-    tv.mute()
-    tv.volume_up()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 2"
-    for _ in range(3):
-        tv.volume_up()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 2"
-
-
-def test_volume_down():
-    tv = Television()
-    tv.volume_down()
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
-    tv.power()
-    tv.volume_down()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-    tv.volume_up()
-    tv.volume_up()
-    tv.volume_up()
-    tv.volume_down()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 1"
-    tv.mute()
-    tv.volume_down()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
-    for _ in range(3):
-        tv.volume_down()
-    assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+    def test_volume_down(self):
+        self.tv.volume_down()
+        assert str(self.tv) == "Power = False, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.volume_down()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_down()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 1"
+        self.tv.mute()
+        self.tv.volume_down()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
+        for _ in range(3):
+            self.tv.volume_down()
+        assert str(self.tv) == "Power = True, Channel = 0, Volume = 0"
 
 if __name__ == "__main__":
     pytest.main(["-v", "--tb=line", "-rN", "-s",__file__])
